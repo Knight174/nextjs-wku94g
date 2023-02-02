d4f33a3a1eaa0c303a9c41f95b5ea6c3389acd32
Knight174 <cpc4253@163.com>
2023-02-03 01:07:10 +0800

    global css, partial css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import styles from './styles/Posts.module.css';
 
 export default function X() {
   return (
     <div>
-      <h1>First Post</h1>
+      <h1 className={styles['text-center']}>First Post</h1>
       <Link href="/">回到首页</Link>
     </div>
   );
--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 export default function Y() {
   return (
     <div>
+      <style jsx>{`h1 {color: #ff4200;}`}</style>
+
       <h1>Second Post</h1>
       <Link href="/">回到首页</Link>
     </div>
