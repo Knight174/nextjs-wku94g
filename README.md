cd90b167ef4ae78d5e1339526ce336ef57f9fd3f
Knight174 <cpc4253@163.com>
2023-02-02 23:00:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-wku94g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-wku94g)
---
+++ pages/posts/first-post.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function X() {
+  return <div>First Post</div>;
+}
