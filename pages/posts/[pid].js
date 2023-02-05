6a63f72e1d4728d3334b69ac89ad27b708a69fe1
Knight174 <cpc4253@163.com>
2023-02-05 19:59:14 +0800

    next/image

--- pages/posts/[pid].js
+++ pages/posts/[pid].js
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
+import postImg from '../../assets/images/1.png';
 
 export default function Post() {
   const router = useRouter();
@@ -17,6 +19,17 @@ export default function Post() {
         <Link href="/posts/post-list">Post List</Link>
       </nav>
       <h1>Post: {pid}</h1>
+      <Image
+        alt="post-image"
+        src={postImg}
+        width={700}
+        height={475}
+        sizes="100vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+      />
     </div>
   );
 }
